5a4f345cb7639976e9005ee833a0e312de58aeab
Kotin(김승호) <naho199345@jinhakapply.com>
2023-08-16 16:12:33 +0900

    delete: Counter Components

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import loadable from "@loadable/component";
 // import Counter from "../features/counter/Counter";
-import Counter from "../features/counter/Counter";
+// import Counter from "../features/counter/Counter";
 
 const Login = loadable(() => import("../pages/login/Login"));
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Counter />} />
+        {/* <Route path="/" element={<Counter />} /> */}
         <Route path="/login/infoUniv/:univCode" element={<Login />} />
       </Routes>
     </BrowserRouter>
